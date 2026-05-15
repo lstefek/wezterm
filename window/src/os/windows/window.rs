1303bc4a5dd3306f6ec9573f36d98a7ac61834d4
lstefek <libor.stefek@seznam.cz>
2026-05-15 14:53:16 +0200

    Trigger CI

--- window/src/os/windows/window.rs
+++ window/src/os/windows/window.rs
@@ -2269,7 +2269,7 @@ impl KeyboardLayoutInfo {
             Modifiers::SHIFT,
             Modifiers::SHIFT | Modifiers::CTRL,
             Modifiers::ALT,
-            Modifiers::RIGHT_ALT, // AltGr
+            Modifiers::RIGHT_ALT,                    // AltGr
             Modifiers::SHIFT | Modifiers::RIGHT_ALT, // Shift-AltGr
         ];
 
